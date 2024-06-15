e072d20a812a1874db191b89df9444729dd3b705
HaMyoungHoon <basicouts@gmail.com>
2024-06-15 11:39:37 +0900

    푸터에 광고 추가

--- src/app/components/landing/footer-section/footer-section.component.ts
+++ src/app/components/landing/footer-section/footer-section.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {afterNextRender, AfterViewInit, ChangeDetectorRef, Component, Inject, Renderer2} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-footer-section',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './footer-section.component.html'
 })
-export class FooterSectionComponent {
+export class FooterSectionComponent implements AfterViewInit {
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer: Renderer2, private cd: ChangeDetectorRef) {
+    afterNextRender(() => {
+      this.cd.markForCheck();
+    });
+  }
 
+  ngAfterViewInit(): void {
+    this.injectScripts();
+  }
+
+  injectScripts(): void {
+    const scriptBody = this.renderer.createElement('script');
+    scriptBody.type = 'text/javascript';
+    scriptBody.text = `(adsbygoogle = window.adsbygoogle || []).push({});`;
+    this.renderer.appendChild(this.document.body, scriptBody);
+  }
 }
