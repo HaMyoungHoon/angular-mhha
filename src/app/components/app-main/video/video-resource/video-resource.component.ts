30c0403bdaef67943dbfcb31a58b797f8f15ccab
HaMyoungHoon <basicouts@gmail.com>
2024-06-16 20:21:39 +0900

    비디오 리소스 추가 중

--- src/app/components/app-main/video/video-resource/video-resource.component.ts
+++ src/app/components/app-main/video/video-resource/video-resource.component.ts
@@ -1,5 +1,11 @@
 import {afterNextRender, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
 import {VideoStreamService} from "../../../../services/rest/video-stream.service";
+import {HttpEvent, HttpEventType} from "@angular/common/http";
+import {FDialogService} from "../../../../services/common/f-dialog.service";
+import {VideoModel} from "../../../../models/rest/file/video/video-model";
+import {getLocalStorage, setLocalStorage} from "../../../../guards/amhohwa";
+import * as FConstants from "../../../../guards/f-constants";
+import {debounceTime, Subject, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-video-resource',
@@ -7,10 +13,20 @@ import {VideoStreamService} from "../../../../services/rest/video-stream.service
   styleUrl: './video-resource.component.scss'
 })
 export class VideoResourceComponent {
-  videoSrc: string
-  @ViewChild('video') video?: ElementRef
-  constructor(private cd: ChangeDetectorRef, private videoStreamService: VideoStreamService) {
-    this.videoSrc = ""
+  volume: number = 0.3;
+  videoSrc: string = "";
+  @ViewChild('videoView') videoView?: ElementRef
+  selectedVideoModel?: VideoModel
+  prevVideoModel?: VideoModel;
+  videoModels?: VideoModel[];
+  videoModelDisable: boolean = false;
+  consoleLog: number = 0;
+  searchLoading: boolean = false;
+  searchValue: string = "";
+  modelChanged: Subject<string> = new Subject<string>();
+  subscription?: Subscription;
+  debounceTime: number = 1000;
+  constructor(private cd: ChangeDetectorRef, private videoStreamService: VideoStreamService, private fDialogService: FDialogService) {
     this.init();
     afterNextRender(() => {
       this.cd.markForCheck();
@@ -21,10 +37,127 @@ export class VideoResourceComponent {
   }
 
   init(): void {
-    const thisIndex = Math.floor(Math.random() * 12 + 1)
-    this.videoSrc = this.videoStreamService.getVideoResourceUrl(thisIndex);
+    this.initVolume();
+    this.initSearch();
+    this.videoStreamService.getVideoList().then(x => {
+      if (x.result) {
+        const videoModels = x.data;
+        if (videoModels === undefined) {
+          return;
+        }
+        this.selectedVideoModel = videoModels[Math.floor(Math.random() * videoModels.length + 1)];
+        this.videoSrc = this.videoStreamService.getVideoResourceUrl(this.selectedVideoModel.thisIndex);
+        return;
+      }
+      this.fDialogService.warn('init', x.msg);
+    }).catch(x => {
+      this.fDialogService.error('init catch', x.message);
+    });
+  }
+  initVolume(): void {
+    let defVolume = +getLocalStorage(FConstants.DEF_VOLUME)
+    if (defVolume === 0 || defVolume > 1) {
+      defVolume = 0.3;
+      setLocalStorage(FConstants.DEF_VOLUME, "0.3");
+    }
+    this.volume = defVolume;
+  }
+  initSearch(): void {
+    this.subscription = this.modelChanged.pipe(debounceTime(this.debounceTime))
+      .subscribe((x) => {
+        this.searchLoading = false;
+        this.searchVideo();
+      });
+  }
+  get videoViewContainerStyle(): string {
+    if (this.videoModels && this.videoModels.length > 0) {
+      return "video-view-container-active";
+    }
+    return "video-view-container";
+  }
+  get videoListContainerStyle(): string {
+    if (this.videoModels && this.videoModels.length > 0) {
+      return "video-list-container-active";
+    }
+    return "video-list-container";
   }
   controlList(): string {
     return "nodownload";
+  }
+  get searchStyle(): string {
+    if (this.searchLoading) return "pi pi-spinner pi-spin";
+    else return "pi pi-search";
+  }
+  videoPlaying(data: any): void {
+  }
+  volumechange(data: any): void {
+    let volume = Math.floor(this.videoView?.nativeElement.volume * 100000) / 100000;
+    if (volume > 1) {
+      volume = 0.3;
+    }
+    setLocalStorage(FConstants.DEF_VOLUME, volume.toString());
+  }
+  searchChange(data: any): void {
+    this.searchLoading = true;
+    this.modelChanged.next(data);
+  }
+  searchVideo(): void {
+    if (this.searchValue.length <= 0) {
+      this.videoModels = [];
+      return;
+    }
+    this.videoModelDisable = true;
+    this.videoStreamService.getVideoSearch(this.searchValue).then(x => {
+      this.videoModelDisable = false;
+      this.videoModels = x.data;
+      if (!x.result) {
+        this.fDialogService.warn('search', x.msg);
+      }
+    }).catch(x => {
+      this.videoModelDisable = false;
+      this.fDialogService.error('search catch', x.message);
+    });
+  }
+  selectVideoModel(data: any): void {
+    if (this.selectedVideoModel === undefined) {
+      this.selectedVideoModel = this.prevVideoModel;
+    }
+
+    this.prevVideoModel = this.selectedVideoModel;
+    if (this.selectedVideoModel) {
+      this.getVideoRequest();
+    }
+  }
+
+  getVideoRequest(): void {
+    if (this.selectedVideoModel === undefined) {
+      return;
+    }
+    this.videoSrc = this.videoStreamService.getVideoResourceUrl(this.selectedVideoModel.thisIndex);
+    return;
+    this.videoStreamService.getVideoResource(this.selectedVideoModel?.thisIndex ?? 1).subscribe({
+      next: (event: HttpEvent<string>) => {
+        switch (event.type) {
+          case HttpEventType.DownloadProgress: {
+            if (this.consoleLog > 10) {
+              return;
+            }
+            this.consoleLog++;
+            console.log(event);
+          }
+            break;
+          case HttpEventType.Response: {
+            console.log(event);
+          }
+            break;
+          default: {
+            console.log(event);
+          }
+        }
+      },
+      error: (x) => {
+        console.log(x);
+      }
+    });
   }
 }
--- src/app/components/app-main/video/video-resource/video-resource.module.ts
+++ src/app/components/app-main/video/video-resource/video-resource.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {VideoResourceComponent} from "./video-resource.component";
 import {VideoResourceRoutingModule} from "./video-resource-routing.module";
+import {AdvertisementComponent} from "../../../common/advertisement/advertisement.component";
+import {FloatLabelModule} from "primeng/floatlabel";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {InputIconModule} from "primeng/inputicon";
+import {IconFieldModule} from "primeng/iconfield";
+import {ListboxModule} from "primeng/listbox";
+import {SkeletonModule} from "primeng/skeleton";
 
 
 
 @NgModule({
   declarations: [VideoResourceComponent],
   imports: [
-    CommonModule, VideoResourceRoutingModule
+    CommonModule, VideoResourceRoutingModule, AdvertisementComponent, FloatLabelModule, InputTextModule, FormsModule, InputIconModule, IconFieldModule, ListboxModule, SkeletonModule
   ]
 })
 export class VideoResourceModule { }
