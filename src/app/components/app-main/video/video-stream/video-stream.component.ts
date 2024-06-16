27357967b4f739c049548b241a0dcfe0790a8f4b
HaMyoungHoon <basicouts@gmail.com>
2024-06-16 20:21:31 +0900

    비디오 스트림 추가 중

--- src/app/components/app-main/video/video-stream/video-stream.component.ts
+++ src/app/components/app-main/video/video-stream/video-stream.component.ts
@@ -1,5 +1,11 @@
-import {afterNextRender, ChangeDetectorRef, Component} from '@angular/core';
+import {afterNextRender, ChangeDetectorRef, Component, ElementRef, ViewChild} from '@angular/core';
 import {VideoStreamService} from "../../../../services/rest/video-stream.service";
+import {Observable, Subject} from "rxjs";
+import {FDialogService} from "../../../../services/common/f-dialog.service";
+import {VideoCategoryModel} from "../../../../models/rest/file/video/video-category-model";
+import {ActivatedRoute} from "@angular/router";
+import {VideoModel} from "../../../../models/rest/file/video/video-model";
+import {VideoViewComponent} from "../../../common/video-view/video-view.component";
 
 @Component({
   selector: 'app-video-stream',
@@ -7,20 +13,79 @@ import {VideoStreamService} from "../../../../services/rest/video-stream.service
   styleUrl: './video-stream.component.scss'
 })
 export class VideoStreamComponent {
-  videoSrc: string
-  constructor(private cd: ChangeDetectorRef, private videoStreamService: VideoStreamService) {
-    this.videoSrc = ""
+  rootDir?: string;
+  @ViewChild('videoView') videoView?: VideoViewComponent;
+  videoCategoryModels?: VideoCategoryModel[];
+  selectedVideoCategoryModel?: VideoCategoryModel;
+  videoModelDisable: boolean;
+  videoModels?: VideoModel[];
+  prevVideoModel?: VideoModel;
+  selectedVideoModel?: VideoModel;
+  constructor(private route: ActivatedRoute, private cd: ChangeDetectorRef, private videoStreamService: VideoStreamService, private fDialogService: FDialogService) {
+//    this.videoSrc = ""
+    route.paramMap.subscribe(x => {
+      this.rootDir = x.get('rootDir') ?? undefined;
+      this.init();
+    });
+    this.videoModelDisable = false;
     afterNextRender(() => {
       this.cd.markForCheck();
-      this.init();
     });
   }
 
   init(): void {
-    const thisIndex = Math.floor(Math.random() * 12 + 1)
-    this.videoSrc = this.videoStreamService.getVideoStreamUrl(thisIndex);
+//    this.videoSrc = this.videoStreamService.getVideoResourceUrl(thisIndex);
+    this.videoStreamService.getCategoryList().then(x => {
+      if (x.result) {
+        const rootBuff = x.data?.filter(x => x.dirName == this.rootDir);
+        if (rootBuff === undefined || rootBuff.length <= 0) {
+          return;
+        }
+        this.videoCategoryModels = rootBuff[0].children;
+        this.selectedVideoCategoryModel = this.videoCategoryModels[0];
+        this.getVideoList();
+        return;
+      }
+      this.fDialogService.warn('init', x.msg);
+    }).catch(x => {
+      this.fDialogService.error("init catch", x.message);
+    });
   }
-  controlList(): string {
-    return "nodownload";
+  selectVideoCategoryModel(data: any): void {
+    this.getVideoList();
+  }
+  getVideoList(): void {
+    const dirName = this.selectedVideoCategoryModel?.dirName;
+    if (dirName === undefined) {
+      return;
+    }
+
+    this.videoModelDisable = true;
+    this.videoStreamService.getVideoByDirname(`${this.rootDir}/${dirName}`).then(x => {
+      if (x.result) {
+        const videoBuff = x.data?.video;
+        if (videoBuff === undefined || videoBuff.length <= 0) {
+          return;
+        }
+        this.videoModels = videoBuff;
+        this.videoModelDisable = false;
+        this.cd.detectChanges();
+        return;
+      }
+      this.fDialogService.warn('videoList', x.msg);
+    }).catch(x => {
+      this.fDialogService.error("videoList catch", x.message);
+      this.videoModelDisable = false;
+    });
+  }
+  selectVideoModel(data: any): void {
+    if (this.selectedVideoModel === undefined || this.selectedVideoCategoryModel === null) {
+      this.selectedVideoModel = this.prevVideoModel;
+    }
+
+    this.prevVideoModel = this.selectedVideoModel;
+    if (this.selectedVideoModel) {
+      this.videoView?.setVideoSrc(this.selectedVideoModel);
+    }
   }
 }
--- src/app/components/app-main/video/video-stream/video-stream.module.ts
+++ src/app/components/app-main/video/video-stream/video-stream.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {VideoStreamComponent} from "./video-stream.component";
 import {VideoStreamRoutingModule} from "./video-stream-routing.module";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
+import {ListboxModule} from "primeng/listbox";
+import {VideoViewComponent} from "../../../common/video-view/video-view.component";
+import {AdvertisementComponent} from "../../../common/advertisement/advertisement.component";
 
 
 
 @NgModule({
   declarations: [VideoStreamComponent],
   imports: [
-    CommonModule, VideoStreamRoutingModule
+    CommonModule, VideoStreamRoutingModule, DropdownModule, FormsModule, ListboxModule, VideoViewComponent, AdvertisementComponent
   ]
 })
 export class VideoStreamModule { }
