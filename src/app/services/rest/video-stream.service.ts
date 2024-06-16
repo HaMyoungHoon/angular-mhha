665d27658c500dca11a961ec3377a84dd8889193
HaMyoungHoon <basicouts@gmail.com>
2024-06-16 20:20:16 +0900

    비디오 스트림 서비스 변경

--- src/app/services/rest/video-stream.service.ts
+++ src/app/services/rest/video-stream.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpResponseInterceptorService} from "../common/http-response-interceptor.service";
+import {IRestResult} from "../../models/common/IRestResult";
+import {VideoCategoryModel} from "../../models/rest/file/video/video-category-model";
+import {VideoModel} from "../../models/rest/file/video/video-model";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +12,37 @@ export class VideoStreamService {
   private videoUrl = "/apiSpring/v1/video";
 
   constructor(private httpResponse: HttpResponseInterceptorService) {
+  }
+
+  getCategoryList(): Promise<IRestResult<VideoCategoryModel[]>> {
+    const url = `${this.videoUrl}/get/category/list`;
+    return this.httpResponse.get(url);
+  }
+  getCategoryRootList(): Promise<IRestResult<VideoCategoryModel[]>> {
+    const url = `${this.videoUrl}/get/category/list/root`;
+    return this.httpResponse.get(url);
+  }
+  getVideoByDirname(dirName: string): Promise<IRestResult<VideoCategoryModel>> {
+    const url = `${this.videoUrl}/get/category/list/withVideo`;
+    this.httpResponse.addParam('dirName', dirName);
+    return this.httpResponse.get(url);
+  }
+  getVideoList(): Promise<IRestResult<VideoModel[]>> {
+    const url = `${this.videoUrl}/get/video/list`;
+    return this.httpResponse.get(url);
+  }
+  getVideoSearch(searchValue: string): Promise<IRestResult<VideoModel[]>> {
+    const url = `${this.videoUrl}/get/video/list/searchString/${searchValue}`;
+    return this.httpResponse.get(url);
+  }
+  getVideoStream(thisIndex: number): Observable<any> {
+    return this.httpResponse.requestAny("get",this.getVideoStreamUrl(thisIndex));
+  }
+  getVideoResource(thisIndex: number): Observable<any> {
+    return this.httpResponse.requestAny("get",this.getVideoResourceUrl(thisIndex));
+  }
+  getVideoByte(thisIndex: number): Observable<any> {
+    return this.httpResponse.requestAny("get", this.getVideoByteUrl(thisIndex));
   }
 
   getVideoStreamUrl(thisIndex: number): string {
