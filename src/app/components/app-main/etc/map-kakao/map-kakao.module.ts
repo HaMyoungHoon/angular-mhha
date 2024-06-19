61446b1565af63705d167ef975072afe58ff7102
HaMyoungHoon <basicouts@gmail.com>
2024-06-19 12:36:27 +0900

    카카오 맵 추가중

--- src/app/components/app-main/etc/map-kakao/map-kakao.module.ts
+++ src/app/components/app-main/etc/map-kakao/map-kakao.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {MapKakaoComponent} from "./map-kakao.component";
+import {MapKakaoRoutingModule} from "./map-kakao-routing.module";
 
 
 
 @NgModule({
   declarations: [MapKakaoComponent],
   imports: [
-    CommonModule
+    CommonModule, MapKakaoRoutingModule
   ]
 })
 export class MapKakaoModule { }
