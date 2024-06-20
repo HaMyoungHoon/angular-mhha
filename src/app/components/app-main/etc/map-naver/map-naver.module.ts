1c3bd403b53255796e27c22e7740e81ff130feb3
HaMyoungHoon <basicouts@gmail.com>
2024-06-21 06:19:03 +0900

    모듈 추가

--- src/app/components/app-main/etc/map-naver/map-naver.module.ts
+++ src/app/components/app-main/etc/map-naver/map-naver.module.ts
@@ -4,13 +4,19 @@ import {MapNaverComponent} from "./map-naver.component";
 import {MapNaverRoutingModule} from "./map-naver-routing.module";
 import {AdvertisementComponent} from "../../../common/advertisement/advertisement.component";
 import {Button} from "primeng/button";
+import {IconFieldModule} from "primeng/iconfield";
+import {InputIconModule} from "primeng/inputicon";
+import {InputTextModule} from "primeng/inputtext";
+import {PaginatorModule} from "primeng/paginator";
+import {ListboxModule} from "primeng/listbox";
+import {CheckboxModule} from "primeng/checkbox";
 
 
 
 @NgModule({
   declarations: [MapNaverComponent],
   imports: [
-    CommonModule, MapNaverRoutingModule, AdvertisementComponent, Button
+    CommonModule, MapNaverRoutingModule, AdvertisementComponent, Button, IconFieldModule, InputIconModule, InputTextModule, PaginatorModule, ListboxModule, CheckboxModule
   ]
 })
 export class MapNaverModule { }
