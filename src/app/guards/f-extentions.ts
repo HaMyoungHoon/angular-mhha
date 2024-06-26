1ffceb7d4de26fccf4507d999f4aac234a8dc8b4
HaMyoungHoon <basicouts@gmail.com>
2024-06-27 06:17:03 +0900

    lambda void func 추가

--- src/app/guards/f-extentions.ts
+++ src/app/guards/f-extentions.ts
@@ -4,3 +4,5 @@ export function awaitDelay(ms: number): Promise<void> {
 export let defLat = 37.5020656;
 export let defLng = 126.8880897;
 export let defPosition = { lat: defLat, lng: defLng };
+
+export type voidFunc = () => void;
