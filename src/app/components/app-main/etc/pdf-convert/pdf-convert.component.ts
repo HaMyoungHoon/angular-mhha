1b7abbe17cac099d84e11d96eb5f9cf0c44bb0b9
HaMyoungHoon <basicouts@gmail.com>
2024-06-28 23:11:27 +0900

    pdf convert component 추가

---
+++ src/app/components/app-main/etc/pdf-convert/pdf-convert.component.ts
@@ -0,0 +1,76 @@
+import {afterNextRender, ChangeDetectorRef, Component, Inject} from '@angular/core';
+import {AspService} from "../../../../services/rest/asp.service";
+import {FDialogService} from "../../../../services/common/f-dialog.service";
+import * as FConstants from "../../../../guards/f-constants";
+import {DownloadFileModel} from "../../../../models/rest/file/download-file-model";
+import {DOCUMENT} from "@angular/common";
+
+@Component({
+  selector: 'app-pdf-convert',
+  templateUrl: './pdf-convert.component.html',
+  styleUrl: './pdf-convert.component.scss'
+})
+export class PdfConvertComponent {
+  maxFileSize: number = 10 * FConstants.KILO * FConstants.KILO;
+  isUploading: boolean = false;
+  downloadFiles: DownloadFileModel[] = [];
+  pdfPassword: string | undefined = undefined;
+  constructor(@Inject(DOCUMENT) private document: Document, private cd: ChangeDetectorRef,
+              private aspService: AspService, private fDialogService: FDialogService) {
+    afterNextRender(() => {
+      this.cd.markForCheck();
+    });
+  }
+
+  upload(event: any, chooseCallback: any): void {
+    chooseCallback();
+  }
+  downloadFile(data: DownloadFileModel): void {
+    const buff = this.document.createElement("A") as HTMLAnchorElement;
+    if (data.url) {
+      buff.href = data.url;
+    } else {
+      return;
+    }
+    if (data.orgFileName) {
+      buff.download = data.orgFileName;
+    }
+    this.document.body.appendChild(buff);
+    buff.click();
+    this.document.body.removeChild(buff);
+  }
+  removeFile(data: DownloadFileModel): void {
+    this.downloadFiles = this.downloadFiles.filter(x => x != data);
+  }
+  onSelectFile(event: any): void {
+    const file = event.currentFiles[0];
+    this.isUploading = true;
+    this.aspService.pdfToWord(file, this.pdfPassword).subscribe(x => {
+      this.isUploading = false;
+      x.body.text().then((y: any): void => {
+        try {
+          this.fDialogService.warn('warn', JSON.parse(y).msg);
+        } catch (_: any) {
+          this.downloadFiles.push(new DownloadFileModel().parseHttpResponse(x));
+        }
+      });
+    });
+  }
+
+  formatSize(bytes: any): string {
+    const fractionDigits = 3
+    const sizes : string[] = FConstants.FILE_SIZE_TYPES;
+    if (bytes === 0) {
+      return `0 ${sizes[0]}`;
+    }
+
+    const i : number = Math.floor(Math.log(bytes) / Math.log(FConstants.KILO));
+    const formattedSize : number = parseFloat((bytes / Math.pow(FConstants.KILO, i)).toFixed(fractionDigits));
+
+    return `${formattedSize} ${sizes[i]}`;
+  }
+
+  get dragDropEnable(): boolean {
+    return !this.isUploading;
+  }
+}
