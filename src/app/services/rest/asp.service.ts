46e66ca7856a207ef3859442d50bf779db8d29f8
HaMyoungHoon <basicouts@gmail.com>
2024-06-28 23:09:59 +0900

    pdf to word convert service 추가

--- src/app/services/rest/asp.service.ts
+++ src/app/services/rest/asp.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import {HttpResponseInterceptorService} from "../common/http-response-interceptor.service";
 import {IRestResult} from "../../models/common/IRestResult";
+import {Observable} from "rxjs";
+import {HttpEvent} from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
@@ -15,5 +17,15 @@ export class AspService {
     this.httpResponse.addParam("id", id);
     this.httpResponse.addParam("pw", pw);
     return this.httpResponse.get(`${this.baseUrl}/User/GetSignIn`).then();
+  }
+
+  pdfToWord(file: any, password: string | undefined = undefined): Observable<any> {
+    let url = `${this.baseUrl}/PdfConverter/PostPdfToWord`;
+    if (password) {
+      url += `?password=${password}`;
+    }
+    const formData = new FormData();
+    formData.append('file', file, file.name);
+    return this.httpResponse.postBlob(url, formData);
   }
 }
