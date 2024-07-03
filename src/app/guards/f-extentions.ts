4c66a455aa0a7865a59383f797695b35d2c8d386
HaMyoungHoon <basicouts@gmail.com>
2024-07-03 19:51:49 +0900

    확장 함수 추가

--- src/app/guards/f-extentions.ts
+++ src/app/guards/f-extentions.ts
@@ -1,3 +1,5 @@
+import Buffer from "buffer"
+
 export function awaitDelay(ms: number): Promise<void> {
   return new Promise(x => setTimeout(x, ms));
 }
@@ -6,3 +8,8 @@ export let defLng = 126.8880897;
 export let defPosition = { lat: defLat, lng: defLng };
 
 export type voidFunc = () => void;
+export type anyFunc = (x: any) => void;
+
+export function toBuffer(data: string): Buffer {
+  return Buffer.Buffer.from(new TextEncoder().encode(data));
+}
